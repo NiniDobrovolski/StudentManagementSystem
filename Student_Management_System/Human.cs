bac4fa6677db39d8d3cc237a8458d0f138dfe5d2
ნინო დობროვოლსკი <n.dobrovolsky.doit@itstep.ge>
2025-12-03 21:46:13 +0400

    Add project files.

---
+++ Student_Management_System/Human.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Student_Management_System;
+
+namespace Student_Management_System
+{
+    public class Human
+    {
+        public string Name { get; private set; }
+        public string Lastname { get; private set; }
+
+        public Human(string name, string lastname)
+        {
+            Name = name;
+            Lastname = lastname;
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+    }
+
+    //Class representing a lecturer
+    public class Lecturer : Human
+    {
+        public int IDNumber { get; private set; }
+        private static int nextID = 1;
+
+        public Lecturer(string name, string lastname) : base(name, lastname)
+        {
+            IDNumber = nextID++; //Assign a unique ID and increment the counter
+        }
+
+      
+        public override string ToString()
+        {
+            return $"{Name} {Lastname}";
+        }
+    }
+
+    //Class representing a student
+    public class Student : Human
+    {
+        public int RollNumber { get; private set; }
+        public char Grade { get;  set; }
+
+        //Constructor with four arguments
+        public Student(string name, string lastname, int rollNumber, char grade) : base(name, lastname)
+        {
+            RollNumber = rollNumber;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Lastname} {RollNumber} {Grade}";
+        }
+    }
+
+}
